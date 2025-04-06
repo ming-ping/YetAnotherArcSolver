961422cb90add928d0e1b77a0ce4a5a10bf4f03c
ming-ping <42094909+ming-ping@users.noreply.github.com>
2025-04-06 14:15:06 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='yarc',
+      version='0.1.0',
+      description='Solve The Abstraction and Reasoning Corpus for Artificial General Intelligence',
+      author='yarc contributors',
+      author_email='mingping765@gmailcom',
+      license='mit',
+      packages=['yarc'],
+      url="https://github.com/ming-ping/YetAnotherArcSolver",
+      install_requires=[
+          'numpy',
+          'scipy',
+          'torch'
+      ],
+      package_data={'yarc': ['models/*.pt', 'models/*.gz']}
+      )
